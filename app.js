9759999c519771ac713a934570e40c7ca18a386f
S R <saranrohit23@gmail.com>
2026-01-17 00:20:06 +0530

    New file changes

---
+++ app.js
@@ -0,0 +1,7 @@
+<head>
+  <body>
+    <h2>Javascript main page</h2>
+    <p id="main> Javascript changes</p>
+      <button type="button" onClick='document.getElementbyId("main").innerHTML = "Hey, User! Welcome!"> Submit </button>
+  </body>
+</head>
